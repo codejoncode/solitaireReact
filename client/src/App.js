1348d358dd37241d977d6033720ab77edcb7fd00
codejoncode <jonathanjholloway@gmail.com>
2018-12-17 01:25:19 -0600

    starting to build a way to flip the card over currently have a way but I need to add a onClick function that will add the classname is flipped to the card so that the flip occurs, I also may want to start each card off on the back side instead of the front like it is currently doing

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,65 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-  state = { 
-    deck: [],
+  state = {
+    deck: []
+  };
+  componentWillMount() {
+    this.setDeckUp();
+    this.shuffleDeck();
   }
-  componentWillMount () {
-    const cards = ["A", " 2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-    const deck = []
-    const suitsLength = 4; // hearts clubs spades diamonds
 
-    for (let i = 0; i< suitsLength; i++){
-      for(let j = 0; j< cards.length; j++){
-        switch (i){
+  setDeckUp = () => {
+    //Step 1 of creating the game solitaire building a deck.
+    const cards = [
+      "A",
+      " 2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    const deck = [];
+    const suitsLength = 4; // hearts clubs spades
+
+    for (let i = 0; i < suitsLength; i++) {
+      for (let j = 0; j < cards.length; j++) {
+        switch (i) {
           case 0:
-            deck.push({"hearts": cards[j]});
+            deck.push({ hearts: cards[j] });
             break;
-          case 1: 
-            deck.push({"clubs": cards[j]});
-            break; 
-          case 2: 
-            deck.push({"spades": cards[j]});
-            break; 
+          case 1:
+            deck.push({ clubs: cards[j] });
+            break;
+          case 2:
+            deck.push({ spades: cards[j] });
+            break;
           case 3:
-            deck.push({"diamonds": cards[j]});
+            deck.push({ diamonds: cards[j] });
             break;
         }
       }
     }
-    this.setState({deck}); 
-  }
-
-  setDeckUp = () => {
-
-  }
+    this.setState({ deck });
+    this.shuffleDeck();
+  };
 
   shuffleDeck = () => {
-    //the purpuse of this function will be to shuffle the deck. 
-    //Instead of importing or using something already written to practice building algorithms in javascript 
+    //Step 2 implement a way to shuffle the deck.
+    //the purpuse of this function will be to shuffle the deck.
+    //Instead of importing or using something already written to practice building algorithms in javascript
     //This function will feature implementation of an algorithm.
 
-    //I will iterate over the entire array and replace it with random indexs. 
-    //This also known as the durstenfeld shuffle algorithm. 
+    //I will iterate over the entire array and replace it with random indexs.
+    //This also known as the durstenfeld shuffle algorithm.
 
     /* Pseduo code to shuffle an array of n elements  
     
@@ -54,28 +72,43 @@ class App extends Component {
 
     The running time of this alogirthm is O(n). 
     */
-    //get a copy of the deck never  manipulate the state directly. 
+    //get a copy of the deck never  manipulate the state directly.
     const deck = this.state.deck.slice();
-    for (let i = deck.length - 1; i > 0;  i--){
-      const j = Math.floor(Math.random() * (i + 1)); 
+    for (let i = deck.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
       const temp = deck[i];
-      deck[i] = deck[j]; 
-      deck[j] = temp; 
+      deck[i] = deck[j];
+      deck[j] = temp;
     }
+  };
 
-  }
+  
 
+  /*
+  Building the layout  one card is face up and six cards is face down next to it. 
+  
+  */
 
   render() {
+    console.log(this.state.deck);
     return (
       <div className="App">
-        
-        <div className="outline">
-          <div className="top"> <span>A</span> <span>&hearts;</span> </div>
-          <h1>&hearts;</h1>
-          <div className="bottom"><span>A</span> <span>&hearts;</span></div>
+        <button onClick={this.shuffleDeck}>New Game</button>
+        <div className="outline scene">
+          <div onClick = {} className="card">
+            <div className="card__face card__face--front">
+              <div className="top">
+                {" "}
+                <span>A</span> <span>&#x2666;</span>{" "}
+              </div>
+              <h1>&#x2666;</h1>
+              <div className="bottom">
+                <span>A</span> <span>&#x2666;</span>
+              </div>
+              <div className="card__face card__face--back">BACK</div>
+            </div>
+          </div>
         </div>
-
       </div>
     );
   }
