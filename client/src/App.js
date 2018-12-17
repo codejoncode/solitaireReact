ec22f42c1ceb765ec551b7aab4a6a8398e4f2d57
codejoncode <jonathanjholloway@gmail.com>
2018-12-17 00:39:29 -0600

    created a shuffle deck function that will be used at the begining of the game and when a user starts the game over. There is one problem I notice with the deck instead of having the actual suit like hears diamonds spades clubs  it actually has suit as the key

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,37 @@ class App extends Component {
     this.setState({deck}); 
   }
 
-  
+  shuffleDeck = () => {
+    //the purpuse of this function will be to shuffle the deck. 
+    //Instead of importing or using something already written to practice building algorithms in javascript 
+    //This function will feature implementation of an algorithm.
+
+    //I will iterate over the entire array and replace it with random indexs. 
+    //This also known as the durstenfeld shuffle algorithm. 
+
+    /* Pseduo code to shuffle an array of n elements  
+    
+    for i from n -1 down to 1 do 
+    j  = random integer  between 0 and n-1 
+    swap  a[j] with a [i]
+
+    Now if your familiar with bubble sort algorithm or any algorithm that features swapping we will have to create a temporary 
+    variable to peform this swap correctly. 
+
+    The running time of this alogirthm is O(n). 
+    */
+    //get a copy of the deck never  manipulate the state directly. 
+    const deck = this.state.deck.slice();
+    for (let i = deck.length - 1; i > 0;  i--){
+      const j = Math.floor(Math.random() * (i + 1)); 
+      const temp = deck[i];
+      deck[i] = deck[j]; 
+      deck[j] = temp; 
+    }
+
+  }
+
+
   render() {
     return (
       <div className="App">
