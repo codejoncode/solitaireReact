dee4a4129dcb409e18319eab5910624bc8604658
codejoncode <jonathanjholloway@gmail.com>
2018-12-17 00:09:15 -0600

    added a state and the string of each card in a suit

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    cards: ["A", " 2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"], 
+  }
   render() {
     return (
       <div className="App">
