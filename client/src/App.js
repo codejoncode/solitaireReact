1b993e1f0fc73646a325b57c68aada959c4a36f7
codejoncode <jonathanjholloway@gmail.com>
2018-12-17 00:22:57 -0600

    built a way to get the deck prepared using componenet will mount

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  state = {
-    cards: ["A", " 2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"], 
+  state = { 
+    deck: [],
   }
+  componentWillMount () {
+    const cards = ["A", " 2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]; 
+    const deck = []
+    const suits = ["hearts", "diamonds", "clubs", "spades"];
+    suits.forEach(suit => {
+      cards.forEach(card => {
+        deck.push({suit: card}); 
+      });
+    });
+    this.setState({deck}); 
+  }
+
+  
   render() {
     return (
       <div className="App">
