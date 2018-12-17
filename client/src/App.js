8171e4708e5f16eb15ac4b1363bac536ac912e71
codejoncode <jonathanjholloway@gmail.com>
2018-12-17 00:01:07 -0600

    starting another project in react attempting to get the card design together

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+        <div className="outline">
+          <div className="top"> <span>A</span> <span>&hearts;</span> </div>
+          <h1>&hearts;</h1>
+          <div className="bottom"><span>A</span> <span>&hearts;</span></div>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
